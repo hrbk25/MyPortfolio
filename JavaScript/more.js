32cc273c7294ab2f8443cf0b60c023d3583fc3b1
hrbk25 <hrbk25@gmail.com>
2023-06-10 15:24:07 +0900

    add: more.css more.js

---
+++ JavaScript/more.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// loading
+
+const following = document.getElementById('following');
+// console.log(followingDot);
+// for(let i = 1; i<=6; i++){
+  const div = document.createElement('div');
+  div.className = 'dot';
+  followingDot.appendChild(div);
+// }
+
+window.onload = function() {
+  const load = document.getElementById('load');
+  load.classList.add('loaded');
+}
